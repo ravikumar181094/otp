f91c750146fb8faa1300799434797ad5f6f8fc69
ravifiresky <76098569+ravifiresky@users.noreply.github.com>
2022-07-21 00:04:04 +0530

    bug fix

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const getRandomPin = (chars, len)=>[...Array(len)].map(
     (i)=>chars[Math.floor(Math.random()*chars.length)]
     ).join('');
     
-function generateOtp(digit=4){
-    return getRandomPin('0123456789',digit);
+function generateOtp(len=4){
+    var chars = "0123456789";
+    return [...Array(len)].map((i)=>chars[Math.floor(Math.random()*chars.length)]).join('');
 }
 
 export default generateOtp;
